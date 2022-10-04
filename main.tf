b2b554d008a3f4602a3fe25125dedd8a46469c6c
Maksym <107770061+5aRaDaY@users.noreply.github.com>
2022-10-05 00:43:09 +0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,6 +1,5 @@
 resource "azurerm_resource_group" "rg" {
-  location = var.resource_group_location
-  name     = var.resource_group_name
+  name     = "final_project_rg"
 }
 
 # Create virtual network
