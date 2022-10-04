c42a4357ffe25e95f9df9d5150c7b39c925a67cb
Maksym <107770061+5aRaDaY@users.noreply.github.com>
2022-10-05 00:43:35 +0300

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,6 +1,3 @@
-output "resource_group_name" {
-  value = azurerm_resource_group.rg.name
-}
 
 output "public_ip_address" {
   value = azurerm_windows_virtual_machine.my_terraform_vm.public_ip_address
@@ -9,4 +6,12 @@ output "public_ip_address" {
 output "tls_private_key" {
   value     = tls_private_key.example_ssh.private_key_pem
   sensitive = true
+}
+
+output "azurerm_mysql_database" {
+  value = azurerm_mysql_database.mysqldb.server_name
+}
+
+output "azurerm_mysql_server" {
+  value = azurerm_mysql_server.mysqlsrv.name
 }
