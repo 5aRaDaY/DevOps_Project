ee98e89b336e7ea7374097ad10f9354d8adc8546
Maksym <107770061+5aRaDaY@users.noreply.github.com>
2022-10-06 04:37:18 +0300

    Update user_details.sql

--- user_details.sql
+++ user_details.sql
@@ -17,7 +17,7 @@ CREATE TABLE IF NOT EXISTS `user_details` (
   `password` varchar(50) DEFAULT NULL,
   `status` tinyint(10) DEFAULT NULL,
   PRIMARY KEY (`user_id`)
-) ENGINE=MyISAM  DEFAULT CHARSET=latin1 AUTO_INCREMENT=10001 ;
+) ENGINE=INNODB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=10001 ;
 
 --
 -- Dumping data for table `user_details`
