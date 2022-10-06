8ac820795620717c38a6432c1627a62b2080e2b9
Maksym <107770061+5aRaDaY@users.noreply.github.com>
2022-10-06 18:59:28 +0300

    Update jenkinsrun.sh

--- script/jenkinsrun.sh
+++ script/jenkinsrun.sh
@@ -1,3 +1,5 @@
 #! /bin/bash
 
+git clone https://github.com/5aRaDaY/HomeTaskRepo.git
+mkdir -p script && cp -R ./HomeTaskRepo/Lesson12/* ./script/
 sudo docker run -d -v script:/container_volume -p 1234:8080 jenkins/jenkins:lts-jdk11
