c35187fc173b57b22f7615f8512452e01e78eeb4
Maksym <107770061+5aRaDaY@users.noreply.github.com>
2022-10-06 04:06:41 +0300

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -7,3 +7,8 @@ variable "resource_group_name" {
   default     = "final_project_rg"
   description = "Name of the resource group."
 }
+
+variable "firewall_rule_ip_address" {
+  default     = "13.73.178.45"
+  description = "VM IP address for firewall rule."
+}
